f665f7a768bb87938bd9af3fc46771bb9e18021b
Torres Moral <ntorcarl@amazon.es>
2025-11-16 03:23:48 +0100

    update CV PDF href

--- src/components/ActionsBar.vue
+++ src/components/ActionsBar.vue
@@ -11,7 +11,7 @@ import ButtonAnchor from "@/components/ButtonAnchor.vue";
     />
     <ButtonAnchor
       label="See as PDF"
-      href="https://raw.githubusercontent.com/carlostxrres/cv/321d954823d733dc42e8a12b9515c7361e54ee86/docs/carlos-torres-cv.pdf"
+      href="https://raw.githubusercontent.com/carlostxrres/cv/9233417bbe69751fb56236b209d1860840dfc440/docs/carlos-torres-cv.pdf"
       icon="pdf"
     />
   </div>

a9e8b40f8f7c7fe8e18795e26e08d7d6956ebb77
Torres Moral <ntorcarl@amazon.es>
2025-12-05 15:54:07 +0100

    update PDF URL

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -1,4 +1,4 @@
 export const resumeGithub = "https://github.com/carlostxrres/cv";
 export const resumeWeb = "https://carlos-resume.netlify.app/";
 export const resumePdf =
-  "https://github.com/carlostxrres/cv/blob/main/public/carlos-torres-cv.pdf";
+  "https://raw.githubusercontent.com/carlostxrres/cv/b9643eb4101fe85760d6c934a714cbc9cb8c573b/public/carlos-torres-cv.pdf";
